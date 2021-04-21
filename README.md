e175a0034599aeecb8254930c9d9a37a01bd0a90
andreguigo <andreguigo@gmail.com>
2021-04-21 18:47:09 -0300

    projeto incial

---
+++ README.md
@@ -0,0 +1,7 @@
+# :key: Gerador de Senhas
+
+### A simples aplicação gera senhas seguras de até 12 caracteres.
+
+O aplicativo faz parte do desenvolvimento de mini projetos pessoais.
+
+[Gerador de Senhas](https://andreguigo.github.io/generate-password_js)
---
+++ index.js
@@ -0,0 +1,51 @@
+let passsize    = document.getElementById('passsize');
+let generate    = document.getElementById('generate');
+let result      = document.getElementById('result');
+let clipboard   = document.getElementById('clipboard');
+
+function generateCode() {
+    let low = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    let upp = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    let num = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    
+    let symbols = '!@#$%&*(){}[]=<>/,.';
+    let sym = symbols[Math.floor(Math.random() * symbols.length)];
+
+    return `${low}${upp}${sym}${num}`;
+}
+
+function generatePassword() {
+    let final = [];
+
+    let x = 0;
+    while (x < passsize.value) {
+        if (passsize.value > 12)
+            return;
+
+        final+= generateCode();
+        x++;
+    }
+
+    result.innerText = final.slice(0, passsize.value);
+}
+
+generate.addEventListener('click', generatePassword);
+
+// copy password for clipboard
+function copyPass() {
+    let copy = result.innerText
+    
+    if(!copy) 
+        return;
+
+    let textarea = document.createElement('textarea');
+    textarea.value = copy;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    
+    alert('Copied to clipboard.');
+}
+
+clipboard.addEventListener('click', copyPass);
