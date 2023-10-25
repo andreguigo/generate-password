7f0870882556deeaf6404f1f1d0dd392f13aeaeb
andreguigo <andreguigo@gmail.com>
2023-10-25 17:07:11 -0300

    refactoring code

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
-# :key: Gerador de Senhas
+# Passwords Generator
 
-### Geração de senhas de forma simples e segura.
+The code is a basic example of generating simple and secure passwords of up to 32 characters.
 
-O código é um exemplo básico de geração de senhas simples e segura.
+## Usage
+
+Use the constant `generatePassword` to output as in the example:
+
+`HTML`
+
+```
+<p id="result"> </p>
+
+<script src="generate-password.js"></script>
+<script>
+    let result = document.getElementById('result');
+    result.innerText = generatePassword;
+</script>
+```
+
+
+Another example of use limiting the number of characters:
+
+`js`
+```
+let limit = 12;
+generatePassword.slice(0, limit);
+```
+
+#### Bonus
+
+The constant `copyClipboard` makes use of the function that copies the result to the desktop. 
+
+To not use this function, comment out the constant in the file.
---
+++ generate-password.js
@@ -0,0 +1,33 @@
+const generatePassword = scalingCode();
+const copyClipboard = copyCode();
+
+function mixCode() {
+    let low = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    let upp = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    let num = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    let symbols = '!@#$%&*(){}[]=<>/,.';
+    let sym = symbols[Math.floor(Math.random() * symbols.length)];
+    let mix = `${low}${upp}${num}${sym}`;
+
+    return mix[Math.floor(Math.random() * mix.length)];
+}
+
+function scalingCode() {
+    let final = [];
+    let x = 0;
+    while (x < 32) {
+        final += mixCode();
+        x++;
+    }
+
+    return final;
+}
+
+// *** bonus *** 
+// copy the generated code to clipboard 
+function copyCode() {
+    let copy = generatePassword;
+    if (!copy)
+        return;
+    navigator.clipboard.writeText(copy);
+}
